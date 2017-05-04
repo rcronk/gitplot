8f0d5bf0aaafbc8241248c90e950ccf93b58d6ab
rcronk <cronk.r@gmail.com>
2017-05-03 23:46:52 -0600

    Got it working, but in the process of getting HEAD added, broke all refs, yay!

--- gitplot.py
+++ gitplot.py
@@ -12,7 +12,7 @@ class Colors(object):
 
 
 type_colors = {}
-object_types = ['tag', 'commit', 'commitsummary', 'tree', 'blob']
+object_types = ['ref', 'tag', 'commit', 'commitsummary', 'tree', 'blob']
 hue_step = 1.0 / len(object_types)
 hue = 0.000
 for object_type in object_types:
@@ -33,141 +33,212 @@ gv.graph_attr['rankdir'] = 'RL'  # Right to left (which makes the first commit o
 
 # repo = git.Repo(r'C:\Users\24860\OneDrive\Personal\Documents\Robert\code\temprepo-jjymki0k')
 repo = git.Repo(r'D:\OneDrive\Personal\Documents\Robert\code\temprepo-jjymki0k')
+# repo = git.Repo(r'C:\Users\cronk\PycharmProjects\mutate')
 # repo = git.Repo(r'C:\Users\24860\code\git\devtools')
 # repo = git.Repo(r'C:\Users\24860\code\git\common')
 # repo = git.Repo('.')
 
-tags = repo.tags
-heads = repo.heads
+# Calculate the length of the short hash based on the total number of objects
+num_objects = 100 # len(objects)
+hash_length = int(math.ceil(math.log(num_objects) * math.log(math.e, 2) / 2))
 
+
+def add_commit(commit):
+    gv.node(commit.hexsha,
+            label=commit.hexsha[:hash_length],
+            color=type_colors[commit.type].line_color,
+            style='filled',
+            fillcolor=type_colors[commit.type].fill_color,
+            penwidth='2',
+            )
+
+
+def add_head(head):
+    gv.node(head.path,
+            label=head.path,
+            color=type_colors['ref'].line_color,
+            style='filled',
+            fillcolor=type_colors['ref'].fill_color,
+            penwidth='2',
+            )
+
+
+def add_sym_ref(name, parent):
+    gv.node(name,
+            label=parent,
+            color=type_colors['ref'].line_color,
+            style='filled',
+            fillcolor=type_colors['ref'].fill_color,
+            penwidth='2',
+            )
+
+
+edges = {}
+def add_edge(git_obj, parent):
+    if type(git_obj) in (git.Head, git.TagReference):
+        if git_obj.name + parent.hexsha not in edges:
+            edges[git_obj.name + parent.hexsha] = None
+            gv.edge(git_obj.name, parent.hexsha, label=str(type(git_obj)))
+    elif type(git_obj) in (git.Commit, git.TagObject):
+        if git_obj.hexsha + parent.hexsha not in edges:
+            edges[git_obj.hexsha + parent.hexsha] = None
+            gv.edge(git_obj.hexsha, parent.hexsha, label='parent')
+    elif type(git_obj) == str:
+        gv.edge(git_obj, parent, label='ref')
+    else:
+        raise Exception('unknown type: %s' % type(git_obj))
+
+
+refs = repo.refs
 objects = []
-for head in heads: # + tags:
-    obj = head.object
-    # Handle object and commit being different here
-    if head.object != head.commit:
-        objects.append(obj.commit)
+for git_obj in refs:
+    if type(git_obj) == git.Head:
+        add_head(git_obj)
+        add_edge(git_obj, git_obj.object)
+        obj = git_obj.object
+    elif type(git_obj) == git.Commit:
+        add_commit(git_obj)
+        obj = git_obj
+    elif type(git_obj) == git.TagReference:
+        add_head(git_obj)
+        add_edge(git_obj, git_obj.object)
+        # If this is an annotated tag, commit and object don't match?
+        if git_obj.object != git_obj.commit:
+            add_commit(git_obj.object)
+            add_commit(git_obj.commit)
+            add_edge(git_obj.object, git_obj.commit)
+        obj = git_obj.commit
+    else:
+        raise Exception('unknown type: %s' % type(git_obj))
     while obj.parents:
         # handle collapsing here
         if obj.hexsha not in [x.hexsha for x in objects]:
-            objects.append(obj)
+            add_commit(obj)
+            add_edge(obj, obj.parents[0])
+            for parent in obj.parents[1:]:
+                if parent.hexsha not in [x.hexsha for x in objects]:
+                    add_edge(obj, parent)
+                    refs.append(parent)  # Follow these other paths later
         obj = obj.parents[0]
-#        if len(obj.parents) > 1:
-#            refs.append(obj.parents[1:])  # Follow these other paths later
+    add_commit(obj)
 
-# Calculate the length of the short hash based on the total number of objects
-num_objects = len(objects)
-hash_length = int(math.ceil(math.log(num_objects) * math.log(math.e, 2) / 2))
+add_sym_ref('HEAD', repo.head.ref.path)
+add_edge('HEAD', repo.head.ref.path)
 
-if collapse_commits:
-    # Trace back from each ref back to the first commit, deleting commits with single parents, etc. to just show
-    # interesting parts of the branching structure.
-    secondary_parents = []
-    processed = 0
-    total = len(heads)
-    for head in heads:
-        print('%s - Finding branch/merge points: %s (%d of %d)' % (time.ctime(), head.name, processed, total))
-        processed += 1
-        if head.name != 'HEAD':
-            commit_list = [x.hexsha for x in objects]
-            obj_index = commit_list.index(head.object.hexsha)
-            obj = objects[obj_index]
+## Calculate the length of the short hash based on the total number of objects
+#num_objects = len(objects)
+#hash_length = int(math.ceil(math.log(num_objects) * math.log(math.e, 2) / 2))
+#
+#if collapse_commits:
+#    # Trace back from each ref back to the first commit, deleting commits with single parents, etc. to just show
+#    # interesting parts of the branching structure.
+#    secondary_parents = []
+#    processed = 0
+#    total = len(heads)
+#    for head in heads:
+#        print('%s - Finding branch/merge points: %s (%d of %d)' % (time.ctime(), head.name, processed, total))
+#        processed += 1
+#        if head.name != 'HEAD':
+#            commit_list = [x.hexsha for x in objects]
+#            obj_index = commit_list.index(head.object.hexsha)
+#            obj = objects[obj_index]
+##            while obj.parents:
+##                if len(obj.parents) != 1:
+##                    for parent in obj.parents[1:]:
+##                        if parent not in secondary_parents:
+##                            secondary_parents.append(old_git.Ref('secondary_head', parent.hexsha))
+##                obj = obj.parents[0].object
+#
+#    objects_to_delete = []
+#    processed = 0
+#    total = len(heads + secondary_parents)
+#    for head in heads + secondary_parents:
+#        print('%s - Collapsing boring commits: %s (%d of %d)' % (time.ctime(), head.name, processed, total))
+#        processed += 1
+#        if head.name != 'HEAD':
+#            obj_index = [x.hexsha for x in objects].index(head.commit.hexsha)
+#            obj = objects[obj_index]
+#            collapsing = False
+#            in_refs = None
+#            num_children = None
+#            num_parents = None
+#            first_commit_id = None
+#            last_obj = None
+#            commits = 0
 #            while obj.parents:
-#                if len(obj.parents) != 1:
-#                    for parent in obj.parents[1:]:
-#                        if parent not in secondary_parents:
-#                            secondary_parents.append(old_git.Ref('secondary_head', parent.hexsha))
-#                obj = obj.parents[0].object
-
-    objects_to_delete = []
-    processed = 0
-    total = len(heads + secondary_parents)
-    for head in heads + secondary_parents:
-        print('%s - Collapsing boring commits: %s (%d of %d)' % (time.ctime(), head.name, processed, total))
-        processed += 1
-        if head.name != 'HEAD':
-            obj_index = [x.hexsha for x in objects].index(head.commit.hexsha)
-            obj = objects[obj_index]
-            collapsing = False
-            in_refs = None
-            num_children = None
-            num_parents = None
-            first_commit_id = None
-            last_obj = None
-            commits = 0
-            while obj.parents:
-                obj_index = [x.hexsha for x in objects].index(obj.hexsha)
-                obj = objects[obj_index]
-                if obj_index not in objects_to_delete and obj.type in ('commit', 'tag'):  #skips summarized and already processed commits
-                    in_refs = len([x for x in heads if x.commit.hexsha == obj.hexsha]) > 0
-                    num_children = len([x for x in objects if obj.hexsha in [y.hexsha for y in x.parents]])
-                    num_parents = len(obj.parents)
-                    if collapsing:
-                        if num_parents == 1 and num_children == 1 and not in_refs:
-                            commits += 1
-                            objects_to_delete.append(obj_index)
-                        else:
-                            if commits == 1:
-                                objects_to_delete.pop()
-                            else:
-                                objects.append(old_git.CommitSummary(first_commit_id, last_obj.hexsha, commits, last_obj.parents))
-                            collapsing = False
-                            first_commit_id = None
-                    else:
-                        if num_parents == 1 and num_children == 1 and not in_refs:
-                            collapsing = True
-                            commits = 1
-                            first_commit_id = obj.hexsha
-                            objects_to_delete.append(obj_index)
-                last_obj = obj
-                obj = obj.parents[0]
-            if collapsing:
-                if num_parents == 1 and num_children == 1 and not in_refs:
-                    commits += 1
-                    objects_to_delete.append(obj_index)
-                objects.append(old_git.CommitSummary(first_commit_id, last_obj.hexsha, commits, last_obj.parents))
-    objects_to_delete = list(set(objects_to_delete))  # Need to reverse sort so we delete from end to start so indexes don't change.
-    objects_to_delete.sort(reverse=True)  # Make unique.
-    processed = 0
-    total = len(objects_to_delete)
-    for index in objects_to_delete:
-        print('%s - Removing boring commits: %s (%d of %d)' % (time.ctime(), objects[index].hexsha, processed, total))
-        processed += 1
-        del objects[index]
-
-processed = 0
-total = len(objects)
-for git_obj in objects:
-    print('%s - Building graph: %d of %d' % (time.ctime(), processed, total))
-    processed += 1
-    if git_obj.type in types_to_include:
-        if git_obj.type == 'commitsummary':
-            label = '%s (%s) %s' % (git_obj.last_commit_id[:hash_length], git_obj.commits, git_obj.hexsha[:hash_length])
-        else:
-            label = git_obj.hexsha[:hash_length]
-        gv.node(git_obj.hexsha,
-                label=label,
-                color=type_colors[git_obj.type].line_color,
-                style='filled',
-                fillcolor=type_colors[git_obj.type].fill_color,
-                penwidth='2',
-                )
-        for parent in git_obj.parents:
-            if parent.type in types_to_include:
-                gv.edge(git_obj.hexsha, parent.hexsha, label=parent.name)
-        for child in git_obj.children:
-            if child.type in types_to_include:
-                gv.edge(git_obj.hexsha, child.hexsha, label=child.name)
-
-if 'ref' in types_to_include:
-    processed = 0
-    total = len(heads)
-    for head in heads:
-        print('%s - Processing heads: %d of %d' % (time.ctime(), processed, total))
-        processed += 1
-        gv.node(head.name, color=type_colors['ref'].line_color,
-                style='filled',
-                fillcolor=type_colors['ref'].fill_color,
-                penwidth='2')
-        gv.edge(head.name, head.hexsha, label='ref')
+#                obj_index = [x.hexsha for x in objects].index(obj.hexsha)
+#                obj = objects[obj_index]
+#                if obj_index not in objects_to_delete and obj.type in ('commit', 'tag'):  #skips summarized and already processed commits
+#                    in_refs = len([x for x in heads if x.commit.hexsha == obj.hexsha]) > 0
+#                    num_children = len([x for x in objects if obj.hexsha in [y.hexsha for y in x.parents]])
+#                    num_parents = len(obj.parents)
+#                    if collapsing:
+#                        if num_parents == 1 and num_children == 1 and not in_refs:
+#                            commits += 1
+#                            objects_to_delete.append(obj_index)
+#                        else:
+#                            if commits == 1:
+#                                objects_to_delete.pop()
+#                            else:
+#                                objects.append(old_git.CommitSummary(first_commit_id, last_obj.hexsha, commits, last_obj.parents))
+#                            collapsing = False
+#                            first_commit_id = None
+#                    else:
+#                        if num_parents == 1 and num_children == 1 and not in_refs:
+#                            collapsing = True
+#                            commits = 1
+#                            first_commit_id = obj.hexsha
+#                            objects_to_delete.append(obj_index)
+#                last_obj = obj
+#                obj = obj.parents[0]
+#            if collapsing:
+#                if num_parents == 1 and num_children == 1 and not in_refs:
+#                    commits += 1
+#                    objects_to_delete.append(obj_index)
+#                objects.append(old_git.CommitSummary(first_commit_id, last_obj.hexsha, commits, last_obj.parents))
+#    objects_to_delete = list(set(objects_to_delete))  # Need to reverse sort so we delete from end to start so indexes don't change.
+#    objects_to_delete.sort(reverse=True)  # Make unique.
+#    processed = 0
+#    total = len(objects_to_delete)
+#    for index in objects_to_delete:
+#        print('%s - Removing boring commits: %s (%d of %d)' % (time.ctime(), objects[index].hexsha, processed, total))
+#        processed += 1
+#        del objects[index]
+#
+#processed = 0
+#total = len(objects)
+#for git_obj in objects:
+#    print('%s - Building graph: %d of %d' % (time.ctime(), processed, total))
+#    processed += 1
+#    if git_obj.type in types_to_include:
+#        if git_obj.type == 'commitsummary':
+#            label = '%s (%s) %s' % (git_obj.last_commit_id[:hash_length], git_obj.commits, git_obj.hexsha[:hash_length])
+#        else:
+#            label = git_obj.hexsha[:hash_length]
+#        gv.node(git_obj.hexsha,
+#                label=label,
+#                color=type_colors[git_obj.type].line_color,
+#                style='filled',
+#                fillcolor=type_colors[git_obj.type].fill_color,
+#                penwidth='2',
+#                )
+#        for parent in git_obj.parents:
+#            if parent.type in types_to_include:
+#                gv.edge(git_obj.hexsha, parent.hexsha, label='parent')
+#        for child in git_obj.children:
+#            if child.type in types_to_include:
+#                gv.edge(git_obj.hexsha, child.hexsha, label='child')
+#
+#if 'ref' in types_to_include:
+#    processed = 0
+#    total = len(heads)
+#    for head in heads:
+#        print('%s - Processing heads: %d of %d' % (time.ctime(), processed, total))
+#        processed += 1
+#        gv.node(head.name, color=type_colors['ref'].line_color,
+#                style='filled',
+#                fillcolor=type_colors['ref'].fill_color,
+#                penwidth='2')
+#        gv.edge(head.name, head.object.hexsha, label='ref')
 
 gv.render('git')
